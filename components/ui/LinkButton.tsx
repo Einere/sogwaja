4b71e8dd548d16875601a4bfe5311ffcaaed5f61
Einere <kjwsx23@gmail.com>
2025-12-07 23:15:51 +0900

    fix: LinkButton 을 원래 목적에 맞춰 단순 a 역할을 수행하도록 수정

--- components/ui/LinkButton.tsx
+++ components/ui/LinkButton.tsx
@@ -5,15 +5,20 @@ import { cn } from "@/lib/utils";
 import { buttonVariants } from "./Button";
 
 export interface LinkButtonProps
-  extends
-    Omit<AnchorHTMLAttributes<HTMLAnchorElement>, "href">,
+  extends Omit<
+      AnchorHTMLAttributes<HTMLAnchorElement>,
+      "href" | "onClick" | "onKeyDown" | "onMouseDown" | "onMouseUp" | "onTouchStart" | "onTouchEnd"
+    >,
     VariantProps<typeof buttonVariants> {
   href: string;
   children: ReactNode;
 }
 
 const LinkButton = forwardRef<HTMLAnchorElement, LinkButtonProps>(
-  ({ href, children, variant, size, className, "aria-disabled": ariaDisabled, ...props }, ref) => {
+  (
+    { href, children, variant, size, className, "aria-disabled": ariaDisabled, tabIndex, ...props },
+    ref
+  ) => {
     const isDisabled = ariaDisabled === true || ariaDisabled === "true";
 
     return (
@@ -22,19 +27,7 @@ const LinkButton = forwardRef<HTMLAnchorElement, LinkButtonProps>(
         ref={ref}
         className={cn(buttonVariants({ variant, size, className }))}
         aria-disabled={ariaDisabled}
-        onClick={e => {
-          if (isDisabled) {
-            e.preventDefault();
-          }
-          props.onClick?.(e);
-        }}
-        onKeyDown={e => {
-          if (isDisabled && (e.key === "Enter" || e.key === " ")) {
-            e.preventDefault();
-          }
-          props.onKeyDown?.(e);
-        }}
-        tabIndex={isDisabled ? -1 : undefined}
+        tabIndex={isDisabled ? -1 : tabIndex}
         {...props}
       >
         {children}
